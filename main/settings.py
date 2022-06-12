6c1be4bf1894a6d2cebfeea8f71acd74d8c802b3
Jiwon Lim <onuonu2222@gmail.com>
2022-06-13 00:49:34 +0900

    Update settings.py

--- main/settings.py
+++ main/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1md$_s_w4+@o@rwe48mp4r7l^0x#$oayc6^4o5@9!o0#qs8-0i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['*', 'local_host']
 CSRF_TRUSTED_ORIGINS = ['http://web.mymincloud.com', 'https://web.mymincloud.com', 'http://tody.mymincloud.com', 'https://tody.mymincloud.com']
 
